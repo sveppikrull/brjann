659613a88f07f2116f7100d3f2f1f1bc13e63afc
Sverrir Kristinsson <sverrirkristinsson@Sverrirs-MacBook-Pro-4.local>
2021-05-22 11:38:09 +0000

    assisted

---
+++ nav.php
@@ -0,0 +1,31 @@
+<nav id="nav">
+    <ul>
+        <li><a href="forsida.html">Home</a></li>
+								<li>      
+									<a href="#">Stigatöflur</a>
+									<ul>
+										<li>
+											<a href="#">Sumar 2021</a>
+											<ul>
+												<li><a href="#">Tour de Brjánn</a></li>
+											    <li><a href="#">Ice in da Veins</a></li>
+                                            </ul>
+										</li>
+									</ul>
+								</li>
+                                <li>    
+									<a href="#">Mót</a>
+									<ul>
+										<li>
+											<a href="#">Sumar 2021</a>
+											<ul>
+												<li><a href="#">Mót 1 - Sveinkotsvöllur</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+                                <li>    
+									<a href="stjorn.html">Stjórn</a>
+								</li>
+							</ul>
+						</nav>
